c4bedeebdd154e37b66355622548f4c63319b64e
zhangyibo <849078974@qq.com>
2022-08-26 22:18:18 +0800

    feat:移除链表倒数第n个节点

---
+++ learn/src/ListNode.java
@@ -0,0 +1,21 @@
+/**
+ * @Author ZhangYiBo
+ * @Date 2022-08-26 21:43
+ */
+public class ListNode {
+
+    Integer val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ learn/src/RemoveNodeList.java
@@ -0,0 +1,22 @@
+/**
+ * @Author ZhangYiBo
+ * @Date 2022-08-26 21:43
+ */
+public class RemoveNodeList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode start = pre, end = pre;
+        while (n != 0) {
+            start = start.next;
+            --n;
+        }
+        while (start.next != null) {
+            start = start.next;
+            end = end.next;
+        }
+        end.next = end.next.next;
+        return pre.next;
+    }
+
+}
