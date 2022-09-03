82de0be7264664384f67b4754a484a1a3c4606f0
xiaozhang <849078974@qq.com>
2022-09-03 20:06:24 +0800

    feat:对称的二叉树

---
+++ learn/src/tree/SymmetryTree.java
@@ -0,0 +1,22 @@
+package tree;
+
+/**
+ * @Author 程序员小张
+ * @Date 2022-09-03 20:02
+ */
+public class SymmetryTree {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left, root.right);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+}
