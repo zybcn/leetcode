ff45dfa7da9decd4704fbdffe0f826c9d3fed5e4
xiaozhang <849078974@qq.com>
2023-01-02 21:13:35 +0800

    feat:数飞机

---
+++ learn/src/base/AirPlane.java
@@ -0,0 +1,57 @@
+package base;
+
+import java.util.*;
+
+/**
+ * @Author 程序员小张
+ * @Date 2023-01-02 20:56
+ * @Desc
+ */
+public class AirPlane {
+
+    private class Node {
+        public int time;
+        public int flag;
+
+        public Node(int time, int flag) {
+            this.time = time;
+            this.flag = flag;
+        }
+    }
+
+    public int countOfAirplanes(List<Interval> airplanes) {
+        int count = 0;
+        List<Node> list = new ArrayList<Node>();
+        for (Interval interval : airplanes) {
+            list.add(new Node(interval.start, 1));
+            list.add(new Node(interval.end, -1));
+        }
+        Collections.sort(list, (a, b) -> a.time != b.time ? a.time - b.time : a.flag - b.flag);
+        int maxPlane = 0;
+        for (Node node : list) {
+            if (node.flag == 1) {
+                count++;
+            } else {
+                count--;
+            }
+            maxPlane = Math.max(maxPlane, count);
+        }
+        return maxPlane;
+    }
+
+
+    public int countOfAirplanes2(List<Interval> airplanes) {
+        TreeMap<Integer, Integer> treemap = new TreeMap<>();
+        for (Interval interval : airplanes) {
+            treemap.put(interval.start, treemap.getOrDefault(interval.start, 0) + 1);
+            treemap.put(interval.end, treemap.getOrDefault(interval.end, 0) - 1);
+        }
+        int maxPlane = 0;
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entity : treemap.entrySet()) {
+            count += entity.getValue();
+            maxPlane = Math.max(maxPlane, count);
+        }
+        return maxPlane;
+    }
+}
---
+++ learn/src/base/Interval.java
@@ -0,0 +1,16 @@
+package base;
+
+/**
+ * @Author 程序员小张
+ * @Date 2023-01-02 20:55
+ * @Desc
+ */
+public class Interval {
+
+    int start, end;
+
+    Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
